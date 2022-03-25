c6d8d806e9549028e0bb55d69d018e5e949131cc
darcos <Daniel.Arcos@outlook.com>
2022-03-25 07:16:39 -0400

    implemented computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log("Hello")
+
+// Rock paper scissors
+// 0: Rock
+// 1: Paper
+// 2: Scissors
+
+// Step 1. write function to generate computer input based on random numbers
+let computerPlay = () => {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+// Step 2. Write function to ask the user for their input?
+// Step 2a. Format user input into standardized lower case
+// Step 2b. See if it is an acceptable answer, if not, end the round
+
+
+const playerSelection = "rock"
+const computerSelection = computerPlay();
+
+console.log("The computer is now guessing")
+console.log(computerGuess)
+
+
+// Step 3. Play a round
+let playRound = (playerSelection, computerSelection) => {
+
+
+}
+
+// Step 4. Declare round winner
